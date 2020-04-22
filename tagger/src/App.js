69c020df69471ec81a996af694519202c852c50e
Milo Rastgoo <msrastgoo@gmail.com>
2020-04-22 16:15:44 -0400

    all frontend stuff moved to electron app

--- tagger/src/App.js
+++ tagger/src/App.js
@@ -1,13 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import TopBar from './components/topbar/TopBar';
+import Sidebar from './components/sidebar/Sidebar';
+import Pagination from './components/pagination/Pagination';
+import EmailList from './components/emailList/EmailList';
+import EmailSection from './components/emailSection/EmailSection';
+import AnalyticsBar from './components/analytics/Analytics';
+import Compose from './components/compose/Compose';
+import './App.scss';
 
-function App() {
+const App = props => {
+
+  const [ composer, setComposer ] = useState(false)
+
   return (
-    <div className="App">
-      <h1>Hello Tagger</h1>
-    </div>
+    <>
+    <TopBar />
+    <main>
+      <Sidebar setComposer={setComposer} />
+      <Pagination />
+      <div className={props.isViewEmail ? 'email-list-min' : 'email-list'}> {/* className="email-list-min" or email-list for full width */}
+          <EmailList setComposer={setComposer}/>
+      </div>
+      {props.isViewEmail && (
+        <div className="email-body" id={props.analyticsBar ? 'email-body-analytics' : null}> {/* add the id="email-body-analytics" for analytics column */}
+          <EmailSection />
+        </div>
+      )}
+      {props.analyticsBar ? <AnalyticsBar /> : null}
+    </main>
+    {composer && <Compose setComposer={setComposer}/>}
+    </>
   );
 }
 
-export default App;
+const mapStateToProps = ({analyticsbar, viewEmail}) => ({
+  analyticsBar:analyticsbar.analyticsbar,
+  isViewEmail:viewEmail.displayEmailSection
+})
+
+export default connect(mapStateToProps)(App);
