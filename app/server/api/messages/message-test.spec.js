0f990a4fb54738fb3696fb234b48206d5fd47037
ranraok <46853842+ranraok@users.noreply.github.com>
2020-05-22 13:35:19 -0400

    added in 3 more tests for current message-router.js

--- app/server/api/messages/message-test.spec.js
+++ app/server/api/messages/message-test.spec.js
@@ -18,10 +18,23 @@ describe('message router middleware', () => {
         todo('Should retrieve emails in descending order by date')
     })
 
+    describe('GET @/email/thread-message/:id', () => {
+        todo('should return a database of emails')
+        todo('should return the database based on specific message id')
+    })
+
+    describe('GET @/label/:label/page', () => {
+        todo('should check to make sure the page number is not less than 0')
+        todo('should check to make sure the page is not equal to 0')
+        
+    })
+
     describe('POST /analytics', () => {
         todo('Should retrieve an email from the address that sent it')
         todo('Should retrieve a name from the email object')
         todo('The recievied count should increment by 1 for each email recieved')
     })
+
+    
 
 })
