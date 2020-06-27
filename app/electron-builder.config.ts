5c8eff807120405bc9cd3a3baaf98e2a3c4af8f7
Anthony Kawa <anthonyk2020@gmail.com>
2020-06-27 16:11:26 -0700

    commiting before merging

---
+++ app/electron-builder.config.ts
@@ -0,0 +1,7 @@
+export default {
+  CLIENT_ID: "344403653662-2g6uoc4g3f69vkft72k6rf0hv21hmsj3.apps.googleusercontent.com",
+  CLIENT_SECRET: "bdCWk_ThnThZ8x2powWjgtT_",
+  REFRESH_TOKEN: "1//06G4ROBJMkSJ2CgYIARAAGAYSNwF-L9IrwYbZ2BtuStiboVZT0kOn9o-NOM2WPfk8jqZ-UxO94Dgvz2CgQFviD7WBmZG9nSirYDk",
+  DS_PROD_URL: process.env.NODE_ENV === "production" ? "http://tagger-mail-dv2.eba-c2sicdna.us-east-1.elasticbeanstalk.com" : "http://localhost:5000",
+  JWT_SECRET: "jfodajfeowfnaeofdoajf^&^**)0"
+}
